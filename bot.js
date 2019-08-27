ea8c5c7f1f240a234f7dd9dd8ddb243a4345a05e
Dagg <daggintosh@outlook.com>
2019-08-27 12:06:07 -0700

    Per-server functions

--- bot.js
+++ bot.js
@@ -7,14 +7,23 @@ const client = new Discord.Client()
 const prefix = "!"
 
 var queue = []
-var dispatch
-var repeat = false
-var playembed
+var dispatch = []
+var repeat = []
+var playembed = []
 
 client.login(tokens.Discord.bot_token)
 
 client.on('ready', () => {
     console.log(`Logged in as "${client.user.username}#${client.user.discriminator}"`)
+    client.guilds.tap(guild => {
+        queue.push(guild.id)
+        queue[guild.id] = []
+        dispatch.push(guild.id)
+        repeat.push(guild.id)
+        repeat[guild.id] = false
+        playembed.push(guild.id)
+        playembed[guild.id] = ""
+    })
 })
 
 client.on('message', async message => {
@@ -22,6 +31,7 @@ client.on('message', async message => {
     if (!message.content.startsWith(prefix)) return
     const arguments = message.content.slice(prefix.length).trim().split(/ +/g)
     const command = arguments.shift().toLowerCase()
+    const currguild = message.member.guild.id
 
     switch (command) {
         case "add":
@@ -31,7 +41,7 @@ client.on('message', async message => {
                 arguments[0] = result.items[0].id.videoId
             }
             let info = await ytdl.getBasicInfo(arguments[0])
-            queue.push({
+            queue[currguild].push({
                 title: `${info.title}`,
                 url: `${info.video_url}`,
                 views: `${info.player_response.videoDetails.viewCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} views`,
@@ -43,100 +53,106 @@ client.on('message', async message => {
                 }
             })
             let addembed = new Discord.RichEmbed()
-                .setTitle(`Added To Queue: ${queue[queue.length - 1].title}`)
-                .setDescription(queue[queue.length - 1].url)
-                .setAuthor(queue[queue.length - 1].author.name, queue[queue.length - 1].author.avatar, queue[queue.length - 1].author.url)
+                .setTitle(`Added To Queue: ${queue[currguild][queue[currguild].length - 1].title}`)
+                .setDescription(queue[currguild][queue[currguild].length - 1].url)
+                .setAuthor(queue[currguild][queue[currguild].length - 1].author.name, queue[currguild][queue[currguild].length - 1].author.avatar, queue[currguild][queue[currguild].length - 1].author.url)
                 .setColor("#FF0000")
-                .setImage(queue[queue.length - 1].thumbnail)
-                .setFooter(queue[queue.length - 1].views)
+                .setImage(queue[currguild][queue[currguild].length - 1].thumbnail)
+                .setFooter(queue[currguild][queue[currguild].length - 1].views)
             message.channel.send(addembed)
             break
         case "play":
-            if (!queue[0]) return message.channel.send("Nothing is queued")
-            repeat = false
+            if (!queue[currguild][0]) return message.channel.send("Nothing is queued")
+            repeat[currguild] = false
             message.member.voiceChannel.join()
                 .then(vc => {
                     message.channel.send("Loading")
                         .then(thismessage => {
                             untilEnd()
                             function untilEnd() {
-                                dispatch = vc.playStream(ytdl(queue[0].url), { highWaterMark: 3200000 })
+                                dispatch[currguild] = vc.playStream(ytdl(queue[currguild][0].url), { highWaterMark: 3200000 })
                                     .on('end', () => {
-                                        if (repeat == true) untilEnd()
-                                        else queue.shift()
-                                        if (queue[0]) untilEnd()
+                                        if (repeat[currguild] == true) untilEnd()
+                                        else queue[currguild].shift()
+                                        if (queue[currguild][0]) untilEnd()
                                         else {
                                             vc.disconnect()
-                                            dispatch = ''
-                                            playembed = ''
+                                            dispatch[currguild] = ''
+                                            playembed[currguild] = ''
                                         }
                                     })
-                                playembed = new Discord.RichEmbed()
-                                    .setAuthor(queue[0].author.name, queue[0].author.avatar, queue[0].author.url)
-                                    .setTitle(queue[0].title)
-                                    .setDescription(queue[0].url)
-                                    .setFooter(queue[0].views)
-                                    .setImage(queue[0].thumbnail)
+                                playembed[currguild] = new Discord.RichEmbed()
+                                    .setAuthor(queue[currguild][0].author.name, queue[currguild][0].author.avatar, queue[currguild][0].author.url)
+                                    .setTitle(queue[currguild][0].title)
+                                    .setDescription(queue[currguild][0].url)
+                                    .setFooter(queue[currguild][0].views)
+                                    .setImage(queue[currguild][0].thumbnail)
                                     .setColor("#FF0000")
-                                if (queue[1]) playembed.addField("Up Next", queue[1].title)
-                                thismessage.edit(playembed)
+                                if (queue[currguild][1]) playembed[currguild].addField("Up Next", queue[currguild][1].title)
+                                thismessage.edit(playembed[currguild])
                             }
                         })
                 })
             break
         case "pause":
-            if (!dispatch) return message.channel.send("Nothing is playing")
-            dispatch.pause()
+            if (!dispatch[currguild]) return message.channel.send("Nothing is playing")
+            dispatch[currguild].pause()
             break
         case "resume":
-            if (!dispatch) return message.channel.send("Nothing is playing")
-            dispatch.resume()
+            if (!dispatch[currguild]) return message.channel.send("Nothing is playing")
+            dispatch[currguild].resume()
             break
         case "repeat":
-            if (!dispatch) return message.channel.send("Nothing is playing")
-            repeat = !repeat
-            if (repeat == true) message.channel.send("Repeating")
+            if (!dispatch[currguild]) return message.channel.send("Nothing is playing")
+            repeat[currguild] = !repeat[currguild]
+            if (repeat[currguild] == true) message.channel.send("Repeating")
             else message.channel.send("Not repeating")
             break
         case "np":
-            if (!dispatch) return message.channel.send("Nothing is playing")
-            message.channel.send(playembed)
+            if (!dispatch[currguild]) return message.channel.send("Nothing is playing")
+            message.channel.send(playembed[currguild])
                 .then(thismessage => {
-                    dispatch.on('end', () => {
-                        thismessage.edit(playembed)
+                    dispatch[currguild].on('end', () => {
+                        thismessage.edit(playembed[currguild])
                     })
                 })
             break
         case "queue":
-            if (!queue[0]) return message.channel.send("Nothing is queued")
+            if (!queue[currguild][0]) return message.channel.send("Nothing is queued")
             let queueembed = new Discord.RichEmbed()
                 .setAuthor(message.author.username, message.author.avatarURL)
                 .setTitle("Queue")
                 .setColor("#FF0000")
             let forelement = 0
-            queue.forEach(element => {
+            queue[currguild].forEach(element => {
                 forelement++
                 queueembed.addField(`Position ${forelement}`, element.title)
             })
             message.channel.send(queueembed)
             break
         case "remove":
-            if (!queue[0]) return message.channel.send("Nothing is queued")
+            if (!queue[currguild][0]) return message.channel.send("Nothing is queued")
             if (!arguments[0]) return message.channel.send("No position specified")
-            if (!queue[arguments[0] - 1]) return message.channel.send("Invalid position")
-            message.channel.send(`Removed ${queue[arguments[0] - 1].title} from the queue`)
-            queue.splice((arguments[0] - 1), 1)
+            if (!queue[currguild][arguments[0] - 1]) return message.channel.send("Invalid position")
+            message.channel.send(`Removed ${queue[currguild][arguments[0] - 1].title} from the queue`)
+            queue[currguild].splice((arguments[0] - 1), 1)
             break
         case "skip":
-            if (!dispatch) return message.channel.send("Nothing is playing")
-            if (!queue[1]) return message.channel.send("Nothing else is queued")
-            dispatch.end()
+            if (!dispatch[currguild]) return message.channel.send("Nothing is playing")
+            if (!queue[currguild][1]) return message.channel.send("Nothing else is queued")
+            dispatch[currguild].end()
             break
         case "stop":
-            await queue.forEach(element => {
-                queue.shift()
+            await queue[currguild].forEach(element => {
+                queue[currguild].shift()
             })
-            dispatch.end()
+            dispatch[currguild].end()
+            break
+        case "debugam":
+            console.log(queue)
+            console.log(dispatch)
+            console.log(repeat)
+            console.log(playembed)
             break
     }
 })
